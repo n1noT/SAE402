6afdd7ccc369bbeb6493d256a3960003b932d119
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-02-07 14:59:30 +0100

    Setup Timer

---
+++ timer.js
@@ -0,0 +1,34 @@
+AFRAME.registerComponent('timer-controller', {
+    init: function () {
+        // Récupérez l'entité du timer
+        var timerEntity = document.getElementById('timer');
+        var commandeEntity = document.getElementById('commande');
+
+        // Initialisez le timer à 10 secondes
+        var timerValue = 10;
+
+        // Mettez à jour le timer chaque seconde
+        this.interval = setInterval(function () {
+            if (timerValue > 0) {
+                timerValue--;
+
+            }
+
+            // Mettez à jour le texte du timer
+            timerEntity.setAttribute('text', 'value', timerValue);
+
+            // Changez la couleur en rouge lorsque le timer atteint 0
+            if (timerValue === 0) {
+                commandeEntity.setAttribute('text', 'value', 'Dommage, je me casse !');
+
+                timerEntity.setAttribute('text', 'color', 'red');
+                timerEntity.setAttribute('text', 'value', '0');
+            }
+        }, 1000);
+    },
+
+    // Assurez-vous de nettoyer l'intervalle lors de la suppression du composant
+    remove: function () {
+        clearInterval(this.interval);
+    },
+});
