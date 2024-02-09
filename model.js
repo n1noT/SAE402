0f8a27ba7c8f2288365fd2238b7d8cdf700e4c53
Mathis Micheau <mathis.micheau@etu.unilim.fr>
2024-02-09 10:48:41 +0100

    V09/02 (NEW KITCHEN + BTN GRILL)

--- model.js
+++ model.js
@@ -1,11 +1,11 @@
 class Ingredient {
     constructor(nom) {
-      this.nom = nom;
+        this.nom = nom;
     }
 }
 
 class IngredientACuire extends Ingredient {
-    constructor(nom,temps, couleur) {
+    constructor(nom, temps, couleur) {
         super(nom);
         this.etat = 'cru';
         this.temps = temps
@@ -22,75 +22,48 @@ class IngredientACuire extends Ingredient {
         console.log(`${this.nom} est cramé !`);
         this.etat = 'crame';
         this.couleur = couleur
-      }
+    }
 
 }
 
 class KitchenElement {
-    constructor(nom){
+    constructor(nom) {
         this.nom = nom;
         this.etat = 'off'
     }
 
-    on(){
+    on() {
         console.log(`${this.nom} is on`);
         this.etat = 'on';
     }
 
-    off(){
+    off() {
         console.log(`${this.nom} is off`);
         this.etat = 'off';
     }
 }
 
-class Plancha extends KitchenElement {
-    constructor(nom, bouton) {
-        super(nom);
-        this.bouton = bouton;
-    }
-
-    // Méthode pour activer la plancha
-    on() {
-        super.on();
-        this.bouton.forEach(id => {
-            const button = document.querySelector(`#${id}`);
-            if (button) {
-                button.setAttribute('material', 'color', '#00FF00'); 
-            }
-        });
-    }
 
 
-    off() {
-        super.off();
-        this.bouton.forEach(id => {
-            const button = document.querySelector(`#${id}`);
-            if (button) {
-                button.setAttribute('material', 'color', '#E92323'); 
-            }
-        });
+// GRILL - BOUTONS
+function handlerClicSurBouton(ev) {
+    let bouton = ev.target;
+    console.log(bouton)
+    if (ev.target.className == 'grill_btn') {
+        if (bouton.dataset.etat == 'off') {
+            bouton.setAttribute('material', 'color : #2ECB2C');
+            bouton.dataset.etat = 'on'
+
+            return
+        }
+        else if (bouton.dataset.etat == 'on') {
+            bouton.setAttribute('material', 'color : #E92323');
+            bouton.dataset.etat = 'off'
+
+            return
+        }
     }
 }
-
-// Fonction pour simuler un clic sur un bouton
-function cliquerSurBouton(idBouton) {
-    let id =  '#' + idBouton;
-    let bouton = document.querySelector(id);
-    if (bouton) {
-        // Change l'état du bouton
-        bouton.setAttribute('material', 'color : #00FF00');
-    } else {
-        console.log(`Le bouton avec l'ID ${idBouton} n'existe pas.`);
-    }
-}
-
-// Créer une instance de la plancha avec les ID des boutons
-const plancha = new Plancha('maPlancha', ['plancha_btn1', 'plancha_btn2', 'plancha_btn3', 'plancha_btn4']);
-
-// Appeler la méthode on() pour activer la plancha
-plancha.on();
-
-// Appeler la fonction pour simuler un clic sur un bouton
 
 
 
@@ -107,24 +80,27 @@ let commandes = [];
 
 // Tableau des recettes que les clients peuvent commander 
 let dataRecette = [
-    {nom: 'burger boeuf', niveau:1, ingredients:[
-        pain, steak.cui(), pain
-    ]
+    {
+        nom: 'burger boeuf', niveau: 1, ingredients: [
+            pain, steak.cui(), fromage,   pain
+        ]
     },
-    {nom: 'burger poulet', niveau:1, ingredients:[
-        pain, poulet.cui(), pain
-    ]
+    {
+        nom: 'burger poulet', niveau: 1, ingredients: [
+            pain, poulet.cui(), pain
+        ]
     },
-    {nom: 'double burger', niveau:2, ingredients:[
-        pain, fromage, steak.cui(), pain, steak, pain
-    ]
+    {
+        nom: 'double burger', niveau: 2, ingredients: [
+            pain, fromage, steak.cui(), pain, steak, pain
+        ]
     },
 ]
 
 // Tableau contenant les 3 assiettes où l'on prépare les commandes
 
 let assiettes = [
-    {num: 1, assiette:[]},
+    { num: 1, assiette: [] },
     // {num: 2, assiette:[]},
     // {num: 3, assiette:[]},
 ]
@@ -133,21 +109,25 @@ let assiettes = [
 // dans le tableau dataRecette et leur niveau associé.
 // La commande est placé dans le tableau commandes contenant toutes les commandes
 
+let commandeClient = function (niveauMax) {
 
-let commandeClient = function(niveauMax){
- 
     let index = Math.floor(Math.random() * dataRecette.length);
 
-    while ( dataRecette[index].niveau > niveauMax){
+    while (dataRecette[index].niveau > niveauMax) {
         index = Math.floor(Math.random() * dataRecette.length);
     }
-    
+
     let commande = dataRecette[index]
 
     commandes.push(commande)
-    
+
 }
 
 commandeClient(1)
 
-cliquerSurBouton('plancha_btn3');
+
+// LISTENER GRILL
+let GRILL = document.querySelectorAll('.grill_btn');
+GRILL.forEach(bouton => {
+    bouton.addEventListener('click', handlerClicSurBouton);
+});
